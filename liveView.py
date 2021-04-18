94b88333994da5a5a54a7102c978803548543005
Rashed Afridi <rashedafridi@gmail.com>
2021-04-19 03:04:23 +0600

    emotion detaction done using keras

---
+++ liveView.py
@@ -0,0 +1,62 @@
+import cv2
+from PIL import Image, ImageTk
+import os
+import numpy as np
+
+import numpy as np
+import cv2
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+from keras.layers import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+
+#loading neuron network model
+emotion_model = Sequential()
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+emotion_model.load_weights('model.h5')
+
+cv2.ocl.setUseOpenCL(False)
+
+emotion_dict = {0: "   Angry   ", 1: "Disgusted", 2: "  Fearful  ", 3: "   Happy   ", 4: "  Neutral  ", 5: "    Sad    ", 6: "Surprised"}
+# start the webcam feed
+cap1 = cv2.VideoCapture(0)  
+show_text=[0]
+while cap1.isOpened():      
+
+    flag1, frame1 = cap1.read()
+    frame1 = cv2.resize(frame1,(600,500))
+
+    bounding_box = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray_frame = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    num_faces = bounding_box.detectMultiScale(gray_frame,scaleFactor=1.3, minNeighbors=5)
+
+    for (x, y, w, h) in num_faces:
+        cv2.rectangle(frame1, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        roi_gray_frame = gray_frame[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray_frame, (48, 48)), -1), 0)
+        prediction = emotion_model.predict(cropped_img)
+        
+        maxindex = int(np.argmax(prediction))
+        cv2.putText(frame1, emotion_dict[maxindex], (x+20, y-60), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        show_text[0]=maxindex
+    if flag1 is None:
+        print (" error!")
+    cv2.imshow('frame', frame1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        exit()
